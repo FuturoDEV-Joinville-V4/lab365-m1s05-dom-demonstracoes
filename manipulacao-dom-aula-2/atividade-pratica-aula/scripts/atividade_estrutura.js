b6303747db1ad721c5d663c813bee379774c923d
Nicholas Macedo <nicholasmacedoo@gmail.com>
2025-07-03 16:57:44 -0300

    feat: Estrutura da aula 2 (Manipulação da DOM)

---
+++ manipulacao-dom-aula-2/atividade-pratica-aula/scripts/atividade_estrutura.js
@@ -0,0 +1,11 @@
+// Olá, Devs! Este é o seu espaço para a Atividade de Criar e Remover Elementos!
+
+// Siga as instruções no arquivo HTML (atividade-estrutura.html).
+
+// Lembre-se:
+// - document.getElementById() para selecionar elementos por ID.
+// - document.createElement('tagName') para criar um novo elemento.
+// - elementoPai.appendChild(elementoFilho) para adicionar.
+// - elementoPai.removeChild(elementoFilho) para remover.
+
+// Boa sorte!
